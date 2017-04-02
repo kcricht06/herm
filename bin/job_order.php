6bf62bc4b0e3a0cd0611c1918e3f0c6820fb6527
Kyle Crichton <kylecrichton@gmail.com>
2017-04-02 13:26:08 -0500

    fixed body of php form

--- bin/job_order.php
+++ bin/job_order.php
@@ -29,7 +29,7 @@ $jobmessage = strip_tags(htmlspecialchars($_POST['jobmessage']));
 // Create the email and send the message
 $to = 'kylecrichton@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Order request:  $name";
-$email_body = "You have received a new job bid request from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nBilling Address: $bill\n\nEmail: $email_address\n\nPhone: $phone\n\nSite: $site";
+$email_body = "You have received a new job bid request from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nBilling Address: $bill\n\nEmail: $email_address\n\nPhone: $phone\n\nSite: $site\n\nCity: $city\n\nCounty: $county\n\nLot: $lot\n\nBlock: $block\n\nSection: $sect\n\nTax Parcel ID: $tax\n\nSpecial Message: $jobmessage";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
